2cb865042840a6dccd11e429c43d1a5c9059b937
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-24 13:05:06 +0300

    add: StringReaderWriter

---
+++ 008_StringReaderWriter/008_StringReaderWriter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _008_StringReaderWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // using System.IO;
+            // Реализует объект System.IO.TextWriter для записи сведений в строку. 
+            // Сведения хранятся в базовом System.Text.StringBuilder.
+
+            StringBuilder sb = new StringBuilder(100);
+            string s = "Расширенная работа со строками: StringReader и StringWriter, изменяемые строки: StringBuilder";
+            sb.Append(s);
+            Console.WriteLine(sb);
+
+            using (StringWriter sw = new StringWriter(sb))
+            {
+                sw.Write("!!!");
+            };
+            Console.WriteLine(sb);
+
+            // Реализует System.IO.TextReader считывает данные из строки.
+            using (StringReader sr = new StringReader(s))
+            {
+                char[] buf = new char[5];
+                sr.ReadBlock(buf, 0, buf.Length);
+            };
+        }
+    }
+}

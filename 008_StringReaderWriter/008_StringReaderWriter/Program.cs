269ec65a171ab2a4f3c53dc96f6550658174356d
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-24 13:25:46 +0300

    fix: design code

--- 008_StringReaderWriter/008_StringReaderWriter/Program.cs
+++ 008_StringReaderWriter/008_StringReaderWriter/Program.cs
@@ -11,22 +11,26 @@ namespace _008_StringReaderWriter
             // using System.IO;
             // Реализует объект System.IO.TextWriter для записи сведений в строку. 
             // Сведения хранятся в базовом System.Text.StringBuilder.
-
             StringBuilder sb = new StringBuilder(100);
+
             string s = "Расширенная работа со строками: StringReader и StringWriter, изменяемые строки: StringBuilder";
+
             sb.Append(s);
+
             Console.WriteLine(sb);
 
             using (StringWriter sw = new StringWriter(sb))
             {
                 sw.Write("!!!");
             };
+
             Console.WriteLine(sb);
 
             // Реализует System.IO.TextReader считывает данные из строки.
             using (StringReader sr = new StringReader(s))
             {
                 char[] buf = new char[5];
+
                 sr.ReadBlock(buf, 0, buf.Length);
             };
         }

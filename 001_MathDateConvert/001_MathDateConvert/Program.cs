0a4b45122af9accd7a64c0ad6ee041b1d8367826
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-24 13:13:27 +0300

    fix: code design

--- 001_MathDateConvert/001_MathDateConvert/Program.cs
+++ 001_MathDateConvert/001_MathDateConvert/Program.cs
@@ -6,39 +6,34 @@ namespace _001_MathDateConvert
     {
         static void Main(string[] args)
         {
-            // Встроенные методы. Класс Math и Convert, DateTime, TimeSpan
-
             #region Convert
             // Преобразует значение одного базового типа данных к другому базовому типу данных.
+            // bool
+            // byte
+            // char
+            // DateTime
+            // decimal
+            // double
+            // short
+            // int
+            // long
+            // sbyte
+            // float
+            // string
+            // ushort
+            // uint
+            // ulong
 
-            //bool
-            //byte
-            //char
-            //DateTime
-            //decimal
-            //double
-            //short
-            //int
-            //long
-            //sbyte
-            //float
-            //string
-            //ushort
-            //uint
-            //ulong
+            // int i = Convert.ToByte("20");
+            // byte j = 0;
 
-            //int i = Convert.ToByte("20");
-            //byte j = 0;
-
-            //j = (byte)int.Parse("2011");
-            //i = Convert.ToByte(12.45);
-            //i = Convert.ToByte(56.78f);
-            //i = Convert.ToByte(89.09m);
-
+            // j = (byte)int.Parse("2011");
+            // i = Convert.ToByte(12.45);
+            // i = Convert.ToByte(56.78f);
+            // i = Convert.ToByte(89.09m);
             #endregion
 
             #region Math
-
             // Console.ReadKey();
             // Предоставляет константы и статические методы для тригонометрических, логарифмических
             // и иных общих математических функций. 
@@ -75,7 +70,6 @@ namespace _001_MathDateConvert
             // Console.WriteLine(Math.Round(-1.5345)); // -2
             // Console.WriteLine(Math.Round(-1.56789,3)); // -1,568
 
-
             // Возвращает целое число, указывающее знак 8-разрядного целого числа со знаком.
             // Console.WriteLine(Math.Sign(-20));  // -1
             // Console.WriteLine(Math.Sign(20));   // 1
@@ -88,8 +82,7 @@ namespace _001_MathDateConvert
             #endregion
 
             #region DateTime TimeSpan
-
-            //Console.ReadKey();
+            // Console.ReadKey();
 
             DateTime date = new DateTime(2020, 09, 28, 01, 30, 00);
             Console.WriteLine($"{date}"); // 28.09.2020 1:30:00
@@ -102,19 +95,20 @@ namespace _001_MathDateConvert
             // date.Second       // Возвращает компонент секунды даты, представленной этим экземпляром.
             // date.Millisecond  // Возвращает компонент миллисекунд для даты, представленной в данном экземпляре. 
 
-            Console.WriteLine(date.ToShortTimeString());        // Получение только времени    1:30
-            Console.WriteLine(date.ToShortDateString());        // Получение только даты       28.09.2020
+            Console.WriteLine(date.ToShortTimeString()); // Получение только времени    1:30
+            Console.WriteLine(date.ToShortDateString()); // Получение только даты       28.09.2020
 
             DateTime newDate = date.AddDays(10);
+
             Console.WriteLine($"{newDate}"); // 08.10.2020 1:30:00
 
             // Console.WriteLine(DateTime.Now);     // Возвращает System.DateTime, которому присвоены текущие дата и время 
             // данного компьютера, выраженные как местное время.
 
             TimeSpan span = newDate - date;
+
             Console.WriteLine(span);                     // 10.00:00:00
             Console.WriteLine(span.TotalMinutes);        // 14400
-
 
             Console.Clear();
 
@@ -130,8 +124,8 @@ namespace _001_MathDateConvert
             Console.WriteLine($"sum = {sum}");
 
             TimeSpan timeSpan = DateTime.Now.Subtract(date);
-            Console.WriteLine($"timeSpan.TotalMilliseconds = {timeSpan.TotalMilliseconds}");
 
+            Console.WriteLine($"timeSpan.TotalMilliseconds = {timeSpan.TotalMilliseconds}");
             #endregion
         }
     }

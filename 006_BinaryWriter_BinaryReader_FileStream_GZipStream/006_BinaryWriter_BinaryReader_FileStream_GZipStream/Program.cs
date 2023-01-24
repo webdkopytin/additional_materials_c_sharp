58f1c8a86f87c6421712e670e00a2c240a1fc029
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-24 13:23:10 +0300

    fix: design code

--- 006_BinaryWriter_BinaryReader_FileStream_GZipStream/006_BinaryWriter_BinaryReader_FileStream_GZipStream/Program.cs
+++ 006_BinaryWriter_BinaryReader_FileStream_GZipStream/006_BinaryWriter_BinaryReader_FileStream_GZipStream/Program.cs
@@ -8,91 +8,86 @@ namespace _006_BinaryWriter_BinaryReader_FileStream_GZipStream
     {
         static void Main(string[] args)
         {
-            //using (StreamReader sr = new StreamReader("db.txt", Encoding.Unicode))
-            //{
-            //    while (!sr.EndOfStream)
-            //    {
-            //        var c = sr.Read();
-            //        //Console.WriteLine($"{c} - {(char)c}");
-            //    }
-            //}
+            // using (StreamReader sr = new StreamReader("db.txt", Encoding.Unicode))
+            // {
+            //     while (!sr.EndOfStream)
+            //     {
+            //         var c = sr.Read();
+            //         //Console.WriteLine($"{c} - {(char)c}");
+            //     }
+            // }
 
-            //Console.WriteLine();
+            // Console.WriteLine();
 
-            //using (StreamReader sr = new StreamReader("db.txt", Encoding.Unicode))
-            //{
-            //    Console.WriteLine(sr.ReadToEnd());
-            //}
+            // using (StreamReader sr = new StreamReader("db.txt", Encoding.Unicode))
+            // {
+            //     Console.WriteLine(sr.ReadToEnd());
+            // }
 
             #region BinaryWriter, BinaryReader
+            // Console.WriteLine($"Нажмите Enter для продолжения\n");
 
-            //Console.WriteLine($"Нажмите Enter для продолжения\n");
+            // Console.ReadKey(); Console.Clear();
 
-            //Console.ReadKey(); Console.Clear();
+            // string[] names = { "Юлия", "Ирина", "Светлана", "Екатерина", "Алиса" };
 
-            //string[] names = { "Юлия", "Ирина", "Светлана", "Екатерина", "Алиса" };
+            // string path = @"BinaryNames.dat";
 
-            //string path = @"BinaryNames.dat";
+            // using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            // {
+            //     // записываем в файл значение каждого поля структуры
+            //     foreach (var name in names)
+            //     {
+            //         writer.Write(name);
+            //     }
+            // }
+            // Console.WriteLine($"Файл {path} успешно создать. Нажмите Enter для продолжения\n");
+            // Console.ReadLine();
 
-            //using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
-            //{
-            //    // записываем в файл значение каждого поля структуры
-            //    foreach (var name in names)
-            //    {
-            //        writer.Write(name);
-            //    }
-            //}
-            //Console.WriteLine($"Файл {path} успешно создать. Нажмите Enter для продолжения\n");
-            //Console.ReadLine();
-
-            //using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
-            //{
-            //    // пока не достигнут конец файла
-            //    // считываем каждое значение из файла
-            //    while (reader.PeekChar() > -1)
-            //    {
-            //        string name = reader.ReadString();
-            //        Console.WriteLine(name);
-            //    }
-            //}
-
+            // using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            // {
+            //     // пока не достигнут конец файла
+            //     // считываем каждое значение из файла
+            //     while (reader.PeekChar() > -1)
+            //     {
+            //         string name = reader.ReadString();
+            //         Console.WriteLine(name);
+            //     }
+            // }
             #endregion
 
             #region FileStream
+            // Console.WriteLine($"Нажмите Enter для продолжения\n");
 
-            //Console.WriteLine($"Нажмите Enter для продолжения\n");
-
-            //Console.ReadKey(); Console.Clear();
-            //Console.Write("Введите строку: ");
-            //string text = Console.ReadLine();
+            // Console.ReadKey(); Console.Clear();
+            // Console.Write("Введите строку: ");
+            // string text = Console.ReadLine();
 
             //// запись в файл
-            //using (FileStream fstream = new FileStream(@"note.txt", FileMode.Create))
-            //{
-            //    byte[] array = System.Text.Encoding.Default.GetBytes(text); // преобразуем строку в байты
-            //    fstream.Write(array, 0, array.Length);                      // запись массива байтов в файл
-            //}
+            // using (FileStream fstream = new FileStream(@"note.txt", FileMode.Create))
+            // {
+            //     byte[] array = System.Text.Encoding.Default.GetBytes(text); // преобразуем строку в байты
+            //     fstream.Write(array, 0, array.Length);                      // запись массива байтов в файл
+            // }
 
             //// чтение из файла
-            //using (FileStream fstream = File.OpenRead(@"note.txt"))
-            //{
+            // using (FileStream fstream = File.OpenRead(@"note.txt"))
+            // {
+            //     byte[] array = new byte[fstream.Length];    // преобразуем строку в байты
+            //
+            //     fstream.Read(array, 0, array.Length);       // считываем данные
+            //
+            //     text = System.Text.Encoding.Default.GetString(array);    // декодируем байты в строку
+            //     Console.WriteLine($"Текст : {text}");
+            // }
 
-            //    byte[] array = new byte[fstream.Length];    // преобразуем строку в байты
-
-            //    fstream.Read(array, 0, array.Length);       // считываем данные
-
-            //    text = System.Text.Encoding.Default.GetString(array);    // декодируем байты в строку
-            //    Console.WriteLine($"Текст : {text}");
-            //}
-
-            //Console.ReadLine();
-
+            // Console.ReadLine();
             #endregion
 
             #region GZipStream
-
             string source = "voina-i-mir.txt";
             string compressed = "voina-i-mir.zip";
+
             using (FileStream ss = new FileStream(source, FileMode.OpenOrCreate))
             {
                 using (FileStream ts = File.Create(compressed))   // поток для записи сжатого файла
@@ -101,6 +96,7 @@ namespace _006_BinaryWriter_BinaryReader_FileStream_GZipStream
                     using (GZipStream cs = new GZipStream(ts, CompressionMode.Compress))
                     {
                         ss.CopyTo(cs); // копируем байты из одного потока в другой
+
                         Console.WriteLine("Сжатие файла {0} завершено. Было: {1}  стало: {2}.",
                                           source,
                                           ss.Length,
@@ -121,6 +117,7 @@ namespace _006_BinaryWriter_BinaryReader_FileStream_GZipStream
                     using (GZipStream ds = new GZipStream(ss, CompressionMode.Decompress))
                     {
                         ds.CopyTo(ts);
+
                         Console.WriteLine($"{source} восстановлен");
 
                         Console.WriteLine("Востановление файла {0} завершено. Было: {1}  стало: {2}.",
